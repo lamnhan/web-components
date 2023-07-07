1165effa1ec7afa4a427fd4c635213cba67e257e
Lam Nhan <nhan.lh@raksul.com>
2023-07-07 21:33:28 +0700

    Lit component

---
+++ src/input.lit.ts
@@ -0,0 +1,110 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+
+import { Validation } from './types';
+
+@customElement('lit-input')
+export class LitInput extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      container-type: inline-size;
+      position: relative;
+    }
+    
+    label {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      font-family: Lucida Sans, Verdana, sans-serif;
+    }
+
+    strong {
+      margin-bottom: 0.5rem;
+      font-size: 2rem;
+    }
+
+    div {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+
+    input {
+      flex: 1;
+      outline: none;
+      box-sizing: border-box;
+      height: 3.5rem;
+      padding: 0.5rem 1rem;
+      background-color: #fff;
+      border: 2px solid #ccc;
+      border-radius: 0.25rem 0 0 0.25rem;
+      font-size: 1.5rem;
+    }
+
+    em {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      box-sizing: border-box;
+      font-style: normal;
+      width: 3.5rem;
+      height: 3.5rem;
+      background-color: #ccc;
+      border-radius: 0 0.25rem 0.25rem 0;
+      text-shadow: 0px 0px 2px #fff;
+      font-size: 3rem;
+    }
+
+    .error input {
+      border-color: var(--color-danger);
+    }
+
+    .error em {
+      background-color: var(--color-danger);
+    }
+
+    .success input {
+      border-color: var(--color-success);
+    }
+
+    .success em {
+      background-color: var(--color-success);
+    }
+  `;
+
+  @property({ type: String }) readonly label = 'Label';
+  @property({ type: Array }) readonly validations?: Validation[];
+  @state() messageType?: string;
+  @state() messageText?: string;
+
+  private onChanged(e: Event) {
+    if (!this.validations) return;
+    const value = (e?.target as HTMLInputElement)?.value;
+    for (let i = 0; i < this.validations.length; i++) {
+      const { equals, type, message } = this.validations[i];
+      const match = value.toLowerCase() === equals.toLowerCase();
+      this.messageType = match ? type : undefined;
+      this.messageText = match ? message : undefined;
+      if (match) break;
+    }
+  }
+
+  protected render() {
+    return html`
+      <label part="input" class=${this.messageType as string}>
+        <strong>${this.label}</strong>
+        <div>
+          <input type="text" @input=${this.onChanged} />
+          <em>${this.messageText || ''}</em>
+        </div>
+      </label>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lit-input': LitInput;
+  }
+}
